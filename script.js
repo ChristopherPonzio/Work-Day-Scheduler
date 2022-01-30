4efb0ae413d6c83a7a81027635d6f2db84b17ba1
Christopher Ponzio <chrisponz11@yahoo.com>
2022-01-30 11:26:32 -0500

    Pull data from local storage to store on document

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function saveText() {
     localStorage.setItem(time, value);
 }
 
+
+//Saves Data to HTML
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
